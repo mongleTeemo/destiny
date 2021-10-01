2d171588ce565dd688cac3a742ffe02a739bd012
seungtae97 <fo_ma@naver.com>
2021-10-01 17:55:29 +0900

    나랑 맞는 핫플 테스트 추가

---
+++ public/js/place.js
@@ -0,0 +1,122 @@
+const SHOW_CL = "is-show",
+  HIDE_CL = "is-hide",
+  swipeZone = document.querySelector(".swipe-zone"),
+  typeZone = document.querySelector(".type-zone"),
+  result1 = document.querySelector(".result.type1"),
+  result2 = document.querySelector(".result.type2"),
+  result3 = document.querySelector(".result.type3"),
+  result4 = document.querySelector(".result.type4"),
+  result5 = document.querySelector(".result.type5"),
+  result6 = document.querySelector(".result.type6"),
+  bottomDefault = document.querySelector(".bottom-default"),
+  loadingArea = document.querySelector(".loding-area");
+let count = 0;
+let loadingCount = 1;
+function countUp() {
+  count = count + 1;
+  console.log(count);
+}
+
+function loadingf() {
+  swipeZone.classList.add(HIDE_CL);
+  typeZone.classList.remove(HIDE_CL);
+  loadingCount++;
+  setTimeout(() => {
+    if (loadingCount < 4) {
+      loadingf();
+    } else {
+      loadingArea.classList.add(HIDE_CL);
+      type();
+    }
+  }, 1000);
+}
+
+function countRset() {
+  count = 0;
+  console.log(count);
+  location.reload();
+}
+
+function type() {
+  let typeCurrent = count;
+
+  if (typeCurrent <= 2) {
+    result1.classList.remove(HIDE_CL);
+    bottomDefault.classList.remove(HIDE_CL);
+  } else if (typeCurrent == 3 || typeCurrent == 4) {
+    result2.classList.remove(HIDE_CL);
+    bottomDefault.classList.remove(HIDE_CL);
+  } else if (typeCurrent == 5 || typeCurrent == 6 || typeCurrent == 7) {
+    result3.classList.remove(HIDE_CL);
+    bottomDefault.classList.remove(HIDE_CL);
+  } else if (typeCurrent == 8 || typeCurrent == 9 || typeCurrent == 10) {
+  }
+}
+
+function shareFacebook() {
+  window.open(
+    "https://www.facebook.com/sharer/sharer.php?u=" +
+      encodeURIComponent(window.location.href)
+  );
+}
+function shareKakao() {
+  const shareButton = document.querySelector("#kakaoLink");
+  shareButton.addEventListener("click", (event) => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "mongle - 카카오톡 유형 검사(KTTI)",
+          url: "https://mongle.io/ktti.html",
+        })
+        .then(() => {
+          console.log("Thanks for sharing!");
+        })
+        .catch(console.error);
+    }
+  });
+}
+function shareCopy(val) {
+  let t = document.createElement("textarea");
+  document.body.appendChild(t);
+  t.value = val;
+  t.select();
+  document.execCommand("copy");
+  document.body.removeChild(t);
+}
+
+$("#copy").click(function () {
+  shareCopy("https://mongle.io/destiny.html");
+  alert("복사되었습니다.");
+});
+
+function checkMobile() {
+  let varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+
+  if (varUA.indexOf("android") > -1) {
+    //안드로이드
+    return "android";
+  } else if (
+    varUA.indexOf("iphone") > -1 ||
+    varUA.indexOf("ipad") > -1 ||
+    varUA.indexOf("ipod") > -1
+  ) {
+    //IOS
+    return "ios";
+  } else {
+    //아이폰, 안드로이드 외
+    return "other";
+  }
+}
+
+function DownLoad() {
+  checkMobile();
+  console.log(checkMobile());
+  const userOs = checkMobile();
+  if (userOs === "android") {
+    window.location.href = "https://subway-snack.com/";
+  } else if (userOs === "ios") {
+    window.location.href = "https://subway-snack.com/";
+  } else {
+    window.location.href = "https://subway-snack.com/";
+  }
+}
